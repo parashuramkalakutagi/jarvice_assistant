51c0bfc32001ff97ddeed4410c0bd11fcd08fe30
parashuramkalakutagi <parashuramkalakutagi9@gmail.com>
2023-10-18 18:15:30 +0530

    jarvice project

---
+++ main.py
@@ -0,0 +1,108 @@
+import speech_recognition as sr
+import os
+import webbrowser
+import openai
+import datetime
+import random
+import numpy as np
+
+import win32com.client
+
+
+api_key = "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"
+
+chatStr = ""
+# https://youtu.be/Z3ZAJoi4x6Q
+def chat(query):
+    global chatStr
+    print(chatStr)
+    openai.api_key = api_key
+    chatStr += f"parshu: {query}\n Jarvis: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
+
+
+def ai(prompt):
+    openai.api_key = api_key
+    text = f"OpenAI response for Prompt: {prompt} \n *************************\n\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    # print(response["choices"][0]["text"])
+    text += response["choices"][0]["text"]
+    if not os.path.exists("Openai"):
+        os.mkdir("Openai")
+
+    # with open(f"Openai/prompt- {random.randint(1, 2343434356)}", "w") as f:
+    with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip() }.txt", "w") as f:
+        f.write(text)
+
+def say(text):
+    speaker = win32com.client.Dispatch("SAPI.SpVoice")
+    speaker.speak(text)
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from Jarvis"
+
+if __name__ == '__main__':
+    print('Welcome to Jarvis A.I')
+    say("Jarvis A.I")
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        # todo: Add more sites
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+        # todo: Add a feature to play a specific song
+
+        if " what is the time" in query.lower():
+            time = datetime.datetime.now().strftime("%H:%M:%S")
+            say(f"Sir time is {time}")
+
+        elif "Using artificial intelligence".lower() in query.lower():
+            ai(prompt=query)
+
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        elif "what is my name".lower() in query.lower():
+            say("sir your name is parashuram and your father name is tammanna and sername is kalakutagi this is the details about you sir ")
+
+        else:
+             say("sir i canot get you please repeat the command")
+             print(query)
